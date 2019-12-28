8fbfea7ec173cbb98bbec3de37f908c4fd8f5109
hyun <rlaalgus132@naver.com>
2019-12-28 14:15:25 +0900

    스플래싀 액티비티 생성   - 토큰 저장 여부 확인 => 어느 화면으로 이동시킬지 결정     - 너무 빨리 동작하는게 어색해서 postDelayed 활용

---
+++ app/src/main/java/com/example/a20191228_01_apipractice/SplashActivity.kt
@@ -0,0 +1,49 @@
+package com.example.a20191228_01_apipractice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.a20191228_01_apipractice.utils.ContextUtil
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setValues()
+        setupEvents()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        openProperActivity()
+
+
+    }
+
+    fun openProperActivity() {
+        Handler().postDelayed(Runnable {
+            if (ContextUtil.getUserToken(mContext) == "") {
+//            로그인 토큰이 저장되지 않았다 => 새로 로그인 할 필요가 있다.
+
+                val intent = Intent(mContext, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+//            로그인 토큰이 빈칸이 아니다 => 저장된 토큰이 있다.
+//            로그인 화면이 아니라, 메인화면으로 보내주자
+
+                val intent = Intent(mContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 1500)
+    }
+
+}
