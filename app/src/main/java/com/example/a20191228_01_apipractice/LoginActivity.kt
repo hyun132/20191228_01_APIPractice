e891c899f32285638ca84c03935bc1d6758e0b51
hyun <rlaalgus132@naver.com>
2019-12-28 13:38:40 +0900

    로그인 요청 후 성공/실패 분기처리

--- app/src/main/java/com/example/a20191228_01_apipractice/LoginActivity.kt
+++ app/src/main/java/com/example/a20191228_01_apipractice/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.a20191228_01_apipractice
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.example.a20191228_01_apipractice.utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
 
@@ -22,17 +25,42 @@ class LoginActivity : BaseActivity() {
 //        3. 둘다 괜찮다면 별개로 분기만 준비.
 
         loginBtn.setOnClickListener {
+            var inputId = idEdt.text.toString()
+            var inputPw = pwEdt.text.toString()
+            if (inputId == "") {
+                Toast.makeText(mContext, "ID를 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (inputPw.length < 8) {
+                Toast.makeText(mContext, "비번이 너무 짧습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+//            Toast.makeText(mContext,"정상 입력이라 로그인 시도해야함",Toast.LENGTH_SHORT).show()
 
-                if(idEdt.text.toString() ==""){
-                    Toast.makeText(mContext,"ID를 입력해주세요",Toast.LENGTH_SHORT).show()
-                    return@setOnClickListener
-                }
-                if (pwEdt.text.toString().length<8){
-                    Toast.makeText(mContext,"비번이 너무 짧습니다.",Toast.LENGTH_SHORT).show()
-                    return@setOnClickListener
-                }
-            Toast.makeText(mContext,"정상 입력이라 로그인 시도해야함",Toast.LENGTH_SHORT).show()
+            ConnectServer.postRequestLogin(
+                mContext,
+                inputId,
+                inputPw,
+                object : ConnectServer.JsonResponseHandler {
 
+                    override fun onResponse(json: JSONObject) {
+                        Log.d("서버응답json", json.toString())
+
+//                    서버에서 돌려주는 code가 몇인지 Int 값 확인
+
+                        val code = json.getInt("code")
+
+                        runOnUiThread {
+                            //               code : 200 이면 로그인 성공 토스트, code : 200이 아니면 로그인 실패 토스트
+                            if (code == 200) {
+                                Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+
+                })
         }
 
     }
