f1d7b8b39c3fa05cec8a22dc8be0b2f56d690961
hyun <rlaalgus132@naver.com>
2019-12-28 14:45:26 +0900

    로그아웃 처리   - 저장된 토큰을 빈칸 ""으로 교체

--- app/src/main/java/com/example/a20191228_01_apipractice/MainActivity.kt
+++ app/src/main/java/com/example/a20191228_01_apipractice/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.a20191228_01_apipractice
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.example.a20191228_01_apipractice.utils.ContextUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -25,6 +27,12 @@ class MainActivity : BaseActivity() {
             alert.setMessage("정말 로그아웃 하시겠습니까?")
             alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
                 Toast.makeText(mContext,"로그아웃",Toast.LENGTH_SHORT).show()
+
+                ContextUtil.setUserToken(mContext,"")
+                var intent = Intent(mContext,LoginActivity::class.java)
+                startActivity(intent)
+
+                finish()
             })
             alert.setNegativeButton("취소", null)
             alert.show()
